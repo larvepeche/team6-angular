57be92fa331625661bfd2a0a6f9b212185d17791
marcus hunald <marcushunald@gmail.com>
2022-03-14 22:01:11 +0300

    Add static inscription

--- src/app/pages/auth/inscription/inscription.component.ts
+++ src/app/pages/auth/inscription/inscription.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-inscription',
-  templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.scss']
+    selector: 'app-inscription',
+    templateUrl: './inscription.component.html',
+    styleUrls: ['./inscription.component.scss']
 })
 export class InscriptionComponent implements OnInit {
+    inscriptionForm = new FormGroup({
+        name: new FormControl(''),
+        email: new FormControl(''),
+        pwd: new FormControl(''),
+        confPwd: new FormControl('')
+    });
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    onConnect() {
 
+    }
 }
