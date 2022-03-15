a13d2820c8e23dc720885a1b59ced5489b568c50
marcus hunald <marcushunald@gmail.com>
2022-03-15 20:18:18 +0300

    Add arrow back to auth page

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-auth',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+    faArrowLeft = faArrowLeft;
 
     isLogin: boolean = true;
 
